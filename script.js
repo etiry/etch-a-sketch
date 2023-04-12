5d1e9e957715067be1f99d589dfeb4f7597fd2b0
etiry <7997357+etiry@users.noreply.github.com>
2023-04-12 16:49:48 -0400

    Add hover effect

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ for (let i = 0; i < 256; i++) {
 
 divs = container.querySelectorAll('div');
 divs.forEach(div => div.classList.add('square'));
+
+container.addEventListener('mouseover', (event) => \
+	event.target.style.backgroundColor = 'black');
