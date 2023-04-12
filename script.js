9a6357ce64119dc62e79fc191e8bc0c80d994fff
etiry <7997357+etiry@users.noreply.github.com>
2023-04-12 16:23:25 -0400

    Add html/js/css for initial grid

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+const container = document.querySelector('#container')
+
+for (let i = 0; i < 256; i++) {
+	const div = document.createElement('div');
+	container.appendChild(div);
+}
+
+divs = container.querySelectorAll('div');
+divs.forEach(div => div.classList.add('square'));
